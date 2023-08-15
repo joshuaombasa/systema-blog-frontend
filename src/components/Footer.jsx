d4d2aebe59a9498c3c3f39daf697fe4ccb2aa3c8
Joshua Ombasa <nyambegaombasa@gmail.com>
2023-08-15 22:34:22 +0300

    added Footer  component

---
+++ src/components/Footer.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+export default function Footer() {
+    return (
+        <footer>© 2023 systema. All Rights Reserved. | Designed and Managed by systema IT</footer>
+    )
+}
