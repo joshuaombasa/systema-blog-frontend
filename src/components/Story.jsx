044b97ef1a924c1c21da1c36cd51bca13fedb800
Joshua Ombasa <nyambegaombasa@gmail.com>
2023-08-20 00:47:35 +0300

    updated component with img compontn

--- src/components/Story.jsx
+++ src/components/Story.jsx
@@ -4,11 +4,13 @@ export default function Story({ blog }) {
     return (
         <div className="story--container">
             <h2>{blog.title}</h2>
+            <img src="../../public/salesforce.jpeg" alt="" />
             <div className="particulars--container">
                 <small>{blog.author_name}</small>
                 <small>{new Date(blog.created_at).toLocaleString()}</small>
             </div>
             <p>{blog.story}</p>
+            <button>Read more...</button>
         </div>
     )
 }
