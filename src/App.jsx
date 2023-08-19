a21ebb1f2823ae6d11bdb8ddc4b904f45f46347c
Joshua Ombasa <nyambegaombasa@gmail.com>
2023-08-20 00:46:05 +0300

    added route to get one image

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import NavLayout from './components/NavLayout'
 import Home from './pages/Home'
 import Login from './pages/Login'
 import Signup from './pages/Signup'
+import Oneblog from './pages/OneBlog'
 function App() {
  
   return (
@@ -15,6 +16,7 @@ function App() {
           <Route path='home' element={<Home/>}/>
           <Route path='login' element={<Login/>}/>
           <Route path='signup' element={<Signup/>}/>
+          <Route path='blog/:id' element={<Oneblog/>}/>
         </Route>
       </Routes>
     </>
