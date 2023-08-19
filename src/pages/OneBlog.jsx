331e45bc5f9469bbcf982a2eb3f93e0652269ace
Joshua Ombasa <nyambegaombasa@gmail.com>
2023-08-20 00:47:05 +0300

    added page to get one story

---
+++ src/pages/OneBlog.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Singlestory from "./Singlestory";
+import Menuparent from "../components/Menuparent";
+
+export default function Oneblog() {
+    return (
+        <div className="one--blog-container">
+            <Singlestory/>
+            <Menuparent/>
+        </div>
+    )
+}
