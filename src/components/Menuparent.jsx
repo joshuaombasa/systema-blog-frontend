684addaaa4f4d50f752318252a09b18d41cc02e0
Joshua Ombasa <nyambegaombasa@gmail.com>
2023-08-20 00:47:57 +0300

    added menu parent

---
+++ src/components/Menuparent.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+
+import MenuItem from "./MenuItem";
+
+export default function Menuparent() {
+
+    const [blogsData, setBlogsData] = React.useState(null)
+
+    React.useEffect(() => {
+        fetch('http://localhost:3000/blogs', {
+            method: 'GET',
+            headers: {
+                'Authorization': localStorage.getItem('token')
+            }
+        })
+            .then(res => res.json())
+            .then(data => {
+                console.log(data)
+                setBlogsData(data)
+            })
+    }, [])
+
+    let blogsJsx
+    if (blogsData) {
+        blogsJsx = blogsData.map(blog => (
+            <MenuItem key={blog.id} blog={blog} />
+        ))
+    }
+
+    
+
+    return (
+        <div className="menu--container">
+            {blogsJsx}
+        </div>
+    )
+}
