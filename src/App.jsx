5b881849a41c1e3f5d7e61b47b9fa0f2a8952a51
Joshua Ombasa <nyambegaombasa@gmail.com>
2023-09-12 10:26:02 +0300

    updated protected routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import { Route, Routes} from 'react-router-dom'
+import { Route, Routes } from 'react-router-dom'
 import './App.css'
 
 import NavLayout from './components/NavLayout'
@@ -9,16 +9,18 @@ import Signup from './pages/Signup'
 import Oneblog from './pages/OneBlog'
 import Write from './pages/Write'
 function App() {
- 
+
   return (
     <>
       <Routes>
-        <Route path='/' element={<NavLayout/>}>
-          <Route path='home' element={<Home/>}/>
-          <Route path='login' element={<Login/>}/>
-          <Route path='signup' element={<Signup/>}/>
-          <Route path='blog/:id' element={<Oneblog/>}/>
-          <Route path='write' element={<Write/>}/>
+        <Route path='/' element={<NavLayout />}>
+          <Route path='login' element={<Login />} />
+          <Route path='home' element={<Home />} />
+          <Route path='signup' element={<Signup />} />
+          <Route path='blog/:id' element={<Oneblog />} />
+          <Route>
+            <Route path='write' element={<Write />} />
+          </Route>
         </Route>
       </Routes>
     </>
