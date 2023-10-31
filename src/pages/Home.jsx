7db584d871d003e1f7a90ecc5d94b3793b291a82
Joshua Ombasa <nyambegaombasa@gmail.com>
2023-11-01 00:19:47 +0300

    updated page logic

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,15 +4,27 @@ import Story from "../components/Story";
 export default function Home() {
 
     const [blogsData, setBlogsData] = React.useState(null)
+    const [loading, setLoading] = React.useState(true)
 
     React.useEffect(() => {
+        setLoading(true)
         fetch('http://localhost:3000/blogs')
         .then(res => res.json())
         .then(data => {
-            console.log(data)
+           
             setBlogsData(data)
         })
+        .catch(error => console.log(error))
+        .finally(setLoading(false))
     }, [])
+
+    if (loading) {
+        return (
+            <div className="home--container">
+                <h1>Loading...</h1>
+            </div>
+        )
+    }
 
     let blogsJsx  
     if (blogsData) {
