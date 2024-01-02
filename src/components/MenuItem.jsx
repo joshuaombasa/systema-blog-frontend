011910db2fed17fc2709182e6ccdf2b019c9457d
Joshua Ombasa <nyambegaombasa@gmail.com>
2024-01-02 15:02:29 +0300

    update page logic

--- src/components/MenuItem.jsx
+++ src/components/MenuItem.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 
+import { Link } from "react-router-dom";
 
 export default function MenuItem({ blog }) {
     return (
-        <div className="story--container menu--item--container">
+        <Link to={`/blog/${blog.id}`} className="story--container menu--item--container">
             <img src="../../public/salesforce.jpeg" alt="" />
             <h2>{blog.title}</h2>
             <button>Read more...</button>
-        </div>
+        </Link>
     )
 }
