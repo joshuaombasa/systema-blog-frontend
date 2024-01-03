b49f7b41104432fb81b59415111fc669e26f3ad4
Joshua Ombasa <nyambegaombasa@gmail.com>
2024-01-03 19:31:24 +0300

    updated component logic

--- src/components/Menuparent.jsx
+++ src/components/Menuparent.jsx
@@ -15,7 +15,6 @@ export default function Menuparent() {
         })
             .then(res => res.json())
             .then(data => {
-                console.log(data)
                 setBlogsData(data)
             })
     }, [])
